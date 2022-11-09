7c33213853edfeab2525f9daceacd7bb98bf0a36
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2022-11-09 08:45:29 +0100

    Added code from ml project

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+from opus_converter import read_opus
+
+files = sorted(os.listdir("./data"))
+for file in files:
+    print(file)
+    data, metadata = read_opus("./data/" + file, metadata=True)
+    print(f"Loaded OPUS file with {data.shape} wavenumber points ranging from {data[0,0]} to {data[-1, 0]}.")
+    for key, val in metadata.items():
+        print(f"{key}: {val}")

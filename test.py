dafee89f0e866dc101eab1b0f5a69c0ebf70097c
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2022-11-10 16:42:04 +0100

    ...

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ from opus_converter import read_opus
 files = sorted(os.listdir("./data"))
 for file in files:
     print(file)
-    data, metadata = read_opus("./data/" + file, metadata=True)
-    print(f"Loaded OPUS file with {data.shape} wavenumber points ranging from {data[0,0]} to {data[-1, 0]}.")
-    for key, val in metadata.items():
-        print(f"{key}: {val}")
+    try:
+        data, metadata = read_opus("./data/" + file, metadata=True)
+    except AttributeError:
+        print("Parsing failed")    
+    else:
+        print(f"Loaded OPUS file with {data.shape} wavenumber points ranging from {data[0,0]} to {data[-1, 0]}.")
+        for key, val in metadata.items():
+            print(f"{key}: {val}")
